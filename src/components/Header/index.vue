73ad26524e478d069f363713bd1d768b2ef6eb33
Gilberto <gilberto@trackmob.com.br>
2020-09-26 18:27:55 -0300

    first commit

---
+++ src/components/Header/index.vue
@@ -0,0 +1,55 @@
+<template>
+<header class="navbar">
+  <section class="navbar">
+    <img alt="Vue logo" src="@/assets/images/logo-medprev.svg">
+  </section>
+  <section class="navbar-section">
+    <nav class="nav-header">
+      <ul>
+        <li>
+          <router-link to="/" class="btn btn-link">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about" class="btn btn-link">Usuários</router-link>
+        </li>
+        <li>
+          <a target="_blank" href="#" class="btn btn-link">GitHub</a>
+        </li>
+      </ul>
+    </nav>
+  </section>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'MHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/sass/_variables.scss';
+
+header.navbar {
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+  background: $light-gray;
+
+  .nav-header ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+
+    a {
+      color: $link-color;
+
+      &:hover {
+        text-decoration: none;
+        opacity: .7;
+      }
+    }
+  }
+}
+</style>
---
+++ src/plugins/webfonts.js
@@ -0,0 +1,7 @@
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Ubuntu:300,400,500,600,800', 'Comfortaa:300,400,500,600,800'],
+  },
+});
